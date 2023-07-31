61fbb3812814526dd5eed361a5ec3e2a2b316053
V-54 <134934669+V-54@users.noreply.github.com>
2023-07-31 23:21:15 +0300

    Create config.py
    Example config file

---
+++ Data/config.py
@@ -0,0 +1,8 @@
+# Update this file with your data
+TOKEN = 'You token from https://t.me/BotFather'
+
+MySQL_db_name = 'VEXOmemory'
+MySQL_db_user = 'root'
+MySQL_db_password = 'You DB password'
+
+admin_chat_id = 1234567890
